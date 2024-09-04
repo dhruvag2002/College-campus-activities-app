fabb562ee933f55690315998ddadf99fee18164d
Tushar <tushar@gmail.com>
2024-09-04 11:39:33 +0530

    hlo

---
+++ new_app/lib/Pages/LoginAndSignup/login.dart
@@ -0,0 +1,241 @@
+import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:new_app/Pages/LoginAndSignup/signup.dart';
+import 'package:new_app/main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../../services/auth.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  late ScaffoldMessengerState scaffoldMessenger;
+
+  @override
+  Widget build(BuildContext context) {
+    scaffoldMessenger = ScaffoldMessenger.of(context);
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: size.height,
+          width: size.width,
+          padding: EdgeInsets.only(top: size.height * 0.1),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromARGB(255, 105, 104, 104),
+                Color.fromARGB(255, 62, 62, 62),
+                Colors.black
+              ],
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Row(
+                children: [
+                  SizedBox(
+                    width: size.width * 0.10,
+                  ),
+                  FadeInUp(
+                    duration: Duration(milliseconds: 800),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: size.height * 0.04),
+              FadeInUp(
+
+                duration: Duration(milliseconds: 1200),
+                child: SizedBox(
+                  width: size.width * 0.8,
+                  height: size.height * 0.08,
+                  child: TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: const TextStyle(color: Colors.white),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Color.fromARGB(255, 150, 150, 150),
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.white,
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    style: const TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              FadeInUp(
+                duration: Duration(milliseconds: 1600),
+                child: SizedBox(
+                  width: size.width * 0.8,
+                  height: size.height * 0.08,
+                  child: TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: const TextStyle(color: Colors.white),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Color.fromARGB(255, 150, 150, 150),
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.white,
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    style: const TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                ),
+              ),
+              SizedBox(height: size.height* 0.03),
+              FadeInUp(
+                duration: Duration(milliseconds: 2000),
+                child: SizedBox(
+                  height: size.height*0.055,
+                  width: 120,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (context.mounted) {
+                        showDialog(
+                          context: context,
+                          barrierDismissible: false,
+                          builder: (BuildContext context) {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          },
+                        );
+                      }
+                      String val = await Auth.signinUser(
+                        emailController.text,
+                        passwordController.text,
+                      );
+                      if (context.mounted) Navigator.pop(context);
+                      if (val == 'success') {
+                        String ptype = await FirebaseFirestore.instance
+                            .collection('users')
+                            .doc(FirebaseAuth.instance.currentUser!.uid)
+                            .get()
+                            .then(
+                              (value) => value.data()!['profileType'].toString(),
+                        );
+
+                        if (context.mounted) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => HomePage(
+                                currentIndex: 1,
+                                profileType: ptype,
+                              ),
+                            ),
+                          );
+                        }
+                        val = 'Signed in';
+                      }
+                      scaffoldMessenger.showSnackBar(
+                        SnackBar(content: Text(val.toString())),
+                      );
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                      MaterialStateProperty.all(Colors.white),
+                    ),
+                    child: const Text(
+                      'Log in',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(height: size.height * 0.15),
+              FadeInUp(
+                duration: Duration(milliseconds: 2400),
+                child: Container(
+                  color: const Color.fromARGB(255, 62, 62, 62),
+                  width: size.width,
+                  height: (size.height * 0.09)+10,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        'Don’t have an account ?',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                      SizedBox(width: size.width*0.01),
+                      InkWell(
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SignupPage(),
+                          ),
+                        ),
+                        child: const Text(
+                          'Sign up',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ new_app/lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:new_app/Pages/LoginAndSignup/login.dart';
+import 'package:new_app/Pages/home/activity.dart';
+import 'package:new_app/Pages/home/admin.dart';
+import 'package:new_app/Pages/home/profile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return FutureBuilder<DocumentSnapshot>(
+                future: FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(FirebaseAuth.instance.currentUser!.uid)
+                    .get(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<DocumentSnapshot> snapshot) {
+                  if (snapshot.hasData) {
+                    return HomePage(
+                      currentIndex: 1,
+                      profileType: snapshot.data!['profileType'].toString(),
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              );
+            } else {
+              return const LoginPage();
+            }
+          }),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final int currentIndex;
+  final String profileType;
+  const HomePage(
+      {super.key, required this.currentIndex, required this.profileType});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late int currentIndex;
+  late String profileType;
+  final List<Widget> _pages = [
+    const ActivityPage(),
+    const ProfilePage(),
+    const AdminPage(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    currentIndex = widget.currentIndex;
+    profileType = widget.profileType;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return WillPopScope(
+      onWillPop: () => Future.value(false),
+      child: Scaffold(
+        body: _pages[currentIndex],
+
+        bottomNavigationBar: CurvedNavigationBar(
+
+          index: currentIndex,
+          height: 57,
+          color: Colors.deepPurpleAccent,
+
+          backgroundColor: Colors.transparent,
+          buttonBackgroundColor: Colors.white,
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 300),
+
+          items: profileType == 'Admin'
+              ? [
+            _buildNavItem(Icons.event, 'Activity'),
+            _buildNavItem(Icons.person, 'Profile'),
+            _buildNavItem(Icons.admin_panel_settings, 'Admin'),
+          ]
+              : [
+            _buildNavItem(Icons.event, 'Activity'),
+            _buildNavItem(Icons.person, 'Profile'),
+          ],
+          onTap: (int index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
+Widget _buildNavItem(IconData icon, String text) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Padding(
+        padding: const EdgeInsets.only(bottom: 1), // Adjust the bottom padding as needed
+        child: Icon(icon, size: 30),
+      ),
+      Text(
+        text,
+        style: TextStyle(fontSize: 12), // Adjust the font size as needed
+      ),
+    ],
+  );
+}
